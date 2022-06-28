639d0fca32c2c1532485fde0ae6189cc3b968276
rob-p <rob.patro@gmail.com>
2022-06-28 15:30:26 -0400

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,15 @@ use std::{
     io::{self, BufReader},
 };
 
-use bio_types::strand::Strand;
 use bio_types::annot::loc::Loc;
 use bio_types::annot::spliced::Spliced;
+use bio_types::strand::Strand;
 use noodles_gtf as gtf;
 use noodles_gtf::record::Strand as NoodlesStrand;
 
 use bio_types::annot::contig::Contig;
+
+use coitrees::{COITree, IntervalNode};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -41,7 +43,7 @@ fn main() -> io::Result<()> {
         let record = result?;
         match record.ty() {
             "exon" => {
-                let s: isize = usize::from(record.start()) as isize;
+                let s: isize = (usize::from(record.start()) as isize) - 1;
                 let e: isize = usize::from(record.end()) as isize;
                 let l: usize = (e - s).try_into().unwrap();
                 let mut t = String::new();
@@ -53,8 +55,8 @@ fn main() -> io::Result<()> {
 
                 let ni = tmap.len();
                 let tid = *tmap.entry(t.clone()).or_insert(ni);
-    
-                // if this is what we just inserted 
+
+                // if this is what we just inserted
                 if ni == tid {
                     tvec.push(Spliced::new(0, 1, 1, Strand::Forward));
                 }
@@ -75,30 +77,52 @@ fn main() -> io::Result<()> {
                 }
                 let ni = tmap.len();
                 let tid = *tmap.entry(t.clone()).or_insert(ni);
-    
-                // if this is what we just inserted 
+
+                // if this is what we just inserted
                 if ni == tid {
                     tvec.push(Spliced::new(0, 1, 1, Strand::Forward));
                 }
-
             }
             _ => {}
         }
     }
 
     let mut txp_to_exon = HashMap::new();
-    
+
     let mut l = 0;
     let mut max_len = 0;
     for (i, e) in evec.iter().enumerate() {
         let mut v = txp_to_exon.entry(e.refid()).or_insert(vec![]);
         v.push(i);
         l = v.len();
-        if l > max_len { max_len = l; }
+        if l > max_len {
+            max_len = l;
+        }
     }
 
-    //for (k,v) in txp_to_exon.iter() {
-    //}
+    let mut txp_features: HashMap<usize, _> = HashMap::new();
+
+    for (k, v) in txp_to_exon.iter_mut() {
+        let strand = evec[v[0]].strand();
+        v.sort_unstable_by_key(|x| evec[*x as usize].start());
+        let s = evec[v[0]].start();
+        let starts: Vec<usize> = v
+            .iter()
+            .map(|e| (evec[*e as usize].start() - s) as usize)
+            .collect();
+        let lens: Vec<usize> = v.iter().map(|e| evec[*e as usize].length()).collect();
+        println!("lens = {:?}, starts = {:?}", lens, starts);
+        txp_features.insert(
+            **k,
+            Spliced::with_lengths_starts(k, s, &lens, &starts, strand).unwrap(),
+        );
+    }
+
+    let interval_vec: Vec<IntervalNode::<usize, usize>> = evec.iter().enumerate().map(|(i, e)|
+                                                                                      IntervalNode::new(e.start() as i32,
+                                                                                      (e.start() + e.length() as isize) as i32,
+                                                                                      i)).collect();
+    let ct = COITree::new(interval_vec);
 
     println!("parsed {} exons", evec.len());
     println!("parsed {} transcripts", tvec.len());
