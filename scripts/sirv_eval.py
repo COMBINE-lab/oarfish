44b0a6b11845573b79bf59b5408adc0ba2405c90
Rob Patro <rob@newton>
2023-06-08 23:17:26 -0400

    all sirv tests

--- scripts/sirv_eval.py
+++ scripts/sirv_eval.py
@@ -12,34 +12,39 @@ def main():
     d = {"SRR6058583": "E2", "SRR6058584": "E0"}
 
     data_dir = "../data"
-    merged = {}
-    for k, v in d.items():
-        o = pd.read_csv(f"{data_dir}/SIRV/quants/oarfish/{k}_quant.tsv", sep="\t")
-        n = pd.read_csv(f"{data_dir}/SIRV/quants/nanocount/{k}_quant.tsv", sep="\s+")
-        merged[v] = pd.merge(o, t, left_on="tname", right_on="Name", how="outer")
-        merged[v] = pd.merge(
-            n, merged[v], left_on="transcript_name", right_on="Name", how="outer"
+    for a in ["c", "i", "o"]:
+        merged = {}
+        for k, v in d.items():
+            o = pd.read_csv(
+                f"{data_dir}/SIRV/quants/oarfish/{k}_{a}_quant.tsv", sep="\t"
+            )
+            n = pd.read_csv(
+                f"{data_dir}/SIRV/quants/nanocount/{k}_{a}_quant.tsv", sep="\s+"
+            )
+            merged[v] = pd.merge(o, t, left_on="tname", right_on="Name", how="outer")
+            merged[v] = pd.merge(
+                n, merged[v], left_on="transcript_name", right_on="Name", how="outer"
+            )
+        print(f"\n============== reference {a} ==================\n")
+        print(f"=== E2 ===\n")
+        print(
+            merged["E2"][["E2", "est_count", "num_reads"]]
+            .corr(method="spearman")
+            .loc["E2", :]
         )
 
-    print(f"\n\n=== E2 ===\n")
-    print(
-        merged["E2"][["E2", "est_count", "num_reads"]]
-        .corr(method="spearman")
-        .loc["E2", :]
-    )
+        print(f"\n=== E0 ===\n")
+        nm = merged["E0"].loc[merged["E0"].E0 == 1, "est_count"].mean()
+        ns = merged["E0"].loc[merged["E0"].E0 == 1, "est_count"].std()
+        om = merged["E0"].loc[merged["E0"].E0 == 1, "num_reads"].mean()
+        os = merged["E0"].loc[merged["E0"].E0 == 1, "num_reads"].std()
+        print(f"NanoCount CV: {ns/nm}\noarfish CV: {os/om}")
 
-    print(f"\n\n=== E0 ===\n")
-    nm = merged["E0"].loc[merged["E0"].E0 == 1, "est_count"].mean()
-    ns = merged["E0"].loc[merged["E0"].E0 == 1, "est_count"].std()
-    om = merged["E0"].loc[merged["E0"].E0 == 1, "num_reads"].mean()
-    os = merged["E0"].loc[merged["E0"].E0 == 1, "num_reads"].std()
-    print(f"nano_count CV: {ns/nm}\noarfish CV: {os/om}")
-
-    nzs = merged["E0"].loc[merged["E0"].E0 == 0, "est_count"].sum()
-    nas = merged["E0"].loc[:, "est_count"].sum()
-    ozs = merged["E0"].loc[merged["E0"].E0 == 0, "num_reads"].sum()
-    oas = merged["E0"].loc[:, "num_reads"].sum()
-    print(f"nano_count % reads mapped: {nzs/nas}\noarfish % reads mapped: {ozs/oas}")
+        nzs = merged["E0"].loc[merged["E0"].E0 == 0, "est_count"].sum()
+        nas = merged["E0"].loc[:, "est_count"].sum()
+        ozs = merged["E0"].loc[merged["E0"].E0 == 0, "num_reads"].sum()
+        oas = merged["E0"].loc[:, "num_reads"].sum()
+        print(f"NanoCount % reads mapped: {nzs/nas}\noarfish % reads mapped: {ozs/oas}")
 
 
 if __name__ == "__main__":
